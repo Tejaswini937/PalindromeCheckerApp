9ae40f58b4685727bcd8fa3d0279dc48c979cb6a
Tejaswini937 <tejaswinireddy0309@gmail.com>
2026-03-11 10:34:39 +0530

    UC6: Palindrome

---
+++ src/UC7.java
@@ -0,0 +1,44 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class UC7 {
+
+    public static boolean isPalindrome(String str) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < str.length(); i++) {
+            deque.addLast(str.charAt(i));
+        }
+
+        // Compare front and rear characters
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
